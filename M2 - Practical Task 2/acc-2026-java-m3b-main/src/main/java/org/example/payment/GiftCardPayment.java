70e6e9a0dfc52dddb2013baebfe7648741780db8
vmedev <vladlensmedvedevs@gmail.com>
2026-06-30 15:52:43 +0300

    GiftCardPayment implementation

--- M2 - Practical Task 2/acc-2026-java-m3b-main/src/main/java/org/example/payment/GiftCardPayment.java
+++ M2 - Practical Task 2/acc-2026-java-m3b-main/src/main/java/org/example/payment/GiftCardPayment.java
@@ -7,12 +7,18 @@ public class GiftCardPayment extends PaymentMethod{
     private double balance;
 
     public GiftCardPayment(String code, double balance) {
-        // TODO: contructor
+        super("GiftCard");
+        this.code = code;
+        this.balance = balance;
+
     }
 
     @Override
     public PaymentResult processPayment(double amount){
-        // TODO: check if balance is enough
-        return new PaymentResult(true, "Paid " + amount + " using gift card");
+        if(balance > amount) {
+            balance -= amount;
+            return new PaymentResult(true, "Paid " + amount + " using gift card");
+        } else
+            return new PaymentResult(false, "Insufficient gift card balance");
     }
 }
